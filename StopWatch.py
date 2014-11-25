266eb9d6041638de2ccb5759021ba7e093590530
DevMoore94 <brmo00206@gmail.com>
2014-11-25 16:07:47 -0400

    Added Miliseconds.

--- StopWatch.py
+++ StopWatch.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty, StringProperty
+import time
 
 kivy.config.Config.set ( 'input', 'mouse', 'mouse,disable_multitouch' )
 
@@ -14,29 +15,54 @@ class StopWatch(Widget):
     #gloabl variables
     seconds = 0
     minuets = 0
-    time_track = StringProperty("00:00")
+    mili = 0
+    time_track = StringProperty("00:00:00")
     running = False
     resetting = True
     timelbl = ObjectProperty(None)    
 
     def update(self,dt):
         self.timelbl.text = str(self.time_track)
+        print(self.mili)
+       
         if(self.running):
 
-            if(self.seconds < 10 and self.minuets < 10):
-                self.time_track = "0" + str(self.minuets) + ":0" + str(self.seconds)
-            elif(self.seconds >= 10 and self.minuets < 10):
-                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds)
-            elif(self.seconds >= 10 and self.minuets >= 10):
-                self.time_track = str(self.minuets) + ":" + str(self.seconds)
+            if(self.seconds < 10 and self.minuets < 10 and self.mili < 10):
+                self.time_track = "0" + str(self.minuets) + ":0" + str(self.seconds) + ":0" + str(self.mili)
+            
+            elif(self.seconds < 10 and self.minuets < 10 and self.mili >= 10):
+                self.time_track = "0" + str(self.minuets) + ":0" + str(self.seconds) + ":" + str(self.mili)
+            
+            elif(self.seconds >= 10 and self.minuets < 10 and self.mili < 10 ):
+                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":0" + str(self.mili)
+
+            elif(self.seconds >= 10 and self.minuets < 10 and self.mili >= 10):
+                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
+
+            elif(self.seconds >= 10 and self.minuets >= 10 and self.mili < 10):
+                self.time_track =  str(self.minuets) + ":" + str(self.seconds) + ":0" + str(self.mili)
+
+            elif(self.seconds < 10 and self.minuets >= 10 and self.mili >= 10):
+                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
+            
+            elif(self.seconds < 10 and self.minuets >= 10 and self.mili < 10):
+                self.time_track = str(self.minuets) + ":0" + str(self.seconds) + ":0" + str(self.mili)
+            
+            elif(self.seconds >= 10 and self.minuets >= 10 and self.mili >= 10):
+                self.time_track = str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
 
             if(self.seconds == 60):
 
                 self.minuets += 1
                 self.seconds = 0
 
-            self.timelbl.text = str(self.time_track)
-            self.seconds+=1
+           
+            if(self.mili == 99):
+                self.seconds+=1
+                self.mili = 0
+
+            self.mili +=1
+            
 
         if(self.resetting == True):
             self.minuets = 0
@@ -64,7 +90,7 @@ class StopWatch(Widget):
 class StopWatchApp(App):     
     def build(self):
         watch = StopWatch()
-        Clock.schedule_interval(watch.update, 1)
+        Clock.schedule_interval(watch.update, 1/100)
         return watch
 
 StopWatchApp().run()
