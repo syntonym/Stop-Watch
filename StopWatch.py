144f4a58ba8d119d02c9b301567f6e134f9478ce
DevMoore94 <brmo00206@gmail.com>
2014-11-26 15:30:28 -0400

    Added Lap Functionality.

--- StopWatch.py
+++ StopWatch.py
@@ -8,8 +8,9 @@ from kivy.properties import ObjectProperty, StringProperty
 import time
 
 kivy.config.Config.set ( 'input', 'mouse', 'mouse,disable_multitouch' )
-kivy.config.Config.set('graphics', 'width', '400')
+kivy.config.Config.set('graphics', 'width', '450')
 kivy.config.Config.set('graphics', 'height', '600')
+
 
 #contains the whole interface
 class StopWatch(Widget):
@@ -19,13 +20,31 @@ class StopWatch(Widget):
     minuets = 0
     mili = 0
     time_track = StringProperty("00:00:00")
+    laps = []
     running = False
     resetting = True
+    addLap = False
+
+    
+
     timelbl = ObjectProperty(None)    
+    laplbl = ObjectProperty(None)
+    
+    def printLaps(self):
+        lap_string = ""
+        count = len(self.laps)
+        for lap in reversed(self.laps):
+            lap_string += "Lap " + str(count) +": " + lap + "\n"
+            count -=1
+        return lap_string   
 
     def update(self,dt):
         self.timelbl.text = str(self.time_track)
-        print(self.mili)
+        
+        if not self.laps:
+            self.laplbl.text = ""
+        else:
+            self.laplbl.text = self.printLaps()
        
         if(self.running):
 
@@ -63,14 +82,21 @@ class StopWatch(Widget):
                 self.mili = 0
 
             self.mili +=1
-            
+           
         if(self.resetting == True):
             self.minuets = 0
             self.seconds = 0
             self.mili = 0
             self.time_track = str(self.minuets) + "0:0" + str(self.seconds) + ":0" + str(self.mili)
+            self.laps = []
             self.resetting = False
             self.running = False
+
+        if(self.addLap == True):
+            self.laps.append(self.time_track)
+            self.laplbl.text = str(self.time_track)
+            self.addLap = False
+           
 
     #Starts the timer
     def start_time(self):
@@ -87,6 +113,12 @@ class StopWatch(Widget):
         print("RESET TIME!")
         self.running = False
         self.resetting = True
+
+    def Lap(self):
+        print("LAP!")
+        self.addLap = True
+        print(self.time_track)
+        
 
 class StopWatchApp(App):     
     def build(self):
